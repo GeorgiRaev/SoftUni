5a22bd5993dbebe478a3ebcde3bdeb3674ae0a10
Georgi Raev <georgihraev89@gmail.com>
2024-11-04 13:59:32 +0200

    Delete folder-Final Project

--- Final Project/F1 App/Configuration/ApiSettings.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace F1_App.Configuration
-{
-    public class ApiSettings
-    {
-        public string DriversApiUrl { get; set; }
-    }
-}
--- Final Project/F1 App/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace F1_App.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
